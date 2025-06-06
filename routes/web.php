b0d698cfa2770df23e452421414bae2dbce99d77
Cletous <ngomacletousjnr@gmail.com>
2025-06-06 12:58:55 +0200

    Scaffold Auth System (Use Laravel Breeze (simple + Livewire-compatible)

--- routes/web.php
+++ routes/web.php
@@ -3,10 +3,21 @@
 use App\Livewire\Patient\Dashboard;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
+Route::view('/', 'welcome');
+
+Route::view('dashboard', 'dashboard')
+    ->middleware(['auth', 'verified'])
+    ->name('dashboard');
+
+Route::view('profile', 'profile')
+    ->middleware(['auth'])
+    ->name('profile');
+
+Route::middleware(['auth'])->group(function () {
+
+    Route::prefix('patient')->as('patient.')->group(function () {
+        Route::get('/dashboard', Dashboard::class)->name('dashboard');
+    });
 });
 
-Route::middleware(['auth'])->prefix('patient')->as('patient.')->group(function () {
-    Route::get('/dashboard', Dashboard::class)->name('dashboard');
-});
+require __DIR__ . '/auth.php';
