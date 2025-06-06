c17bd46d3498c81b3edb42d8613c13ab09c65e2e
Cletous <ngomacletousjnr@gmail.com>
2025-06-06 13:30:38 +0200

    Create Livewire Component for submitting diagnosis

---
+++ app/Livewire/Doctor/SubmitDiagnosis.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire\Doctor;
+
+use Livewire\Component;
+
+class SubmitDiagnosis extends Component
+{
+    public function render()
+    {
+        return view('livewire.doctor.submit-diagnosis');
+    }
+}
