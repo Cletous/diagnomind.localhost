0498e21a73469ec3c87d2bb462037e1f14ff00dc
Cletous <ngomacletousjnr@gmail.com>
2025-06-19 22:04:39 +0200

    Add Verification Routes

--- routes/auth.php
+++ routes/auth.php
@@ -4,6 +4,26 @@ use Illuminate\Support\Facades\Auth;
 
 use App\Livewire\Guest\AuthLivewire;
 
+use Illuminate\Foundation\Auth\EmailVerificationRequest;
+use Illuminate\Http\Request;
+
+Route::middleware('auth')->group(function () {
+    Route::get('/email/verify', function () {
+        return view('auth.verify-email');
+    })->name('verification.notice');
+
+    Route::get('/email/verify/{id}/{hash}', function (EmailVerificationRequest $request) {
+        $request->fulfill();
+        return redirect('/verified-redirect');
+    })->middleware(['signed'])->name('verification.verify');
+
+    Route::post('/email/verification-notification', function (Request $request) {
+        $request->user()->sendEmailVerificationNotification();
+        return back()->with('success', 'Verification link sent!');
+    })->middleware(['throttle:6,1'])->name('verification.send');
+});
+
+
 Route::get('/login', AuthLivewire::class)->name('login');
 Route::get('/register', AuthLivewire::class)->name('register');
 Route::get('/forget-password', AuthLivewire::class)->name('forget_password');
