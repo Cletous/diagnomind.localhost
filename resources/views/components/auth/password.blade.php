a6cd9eb1c3a63e89bd1ca5cb72acc87ccc8c92ce
Cletous <ngomacletousjnr@gmail.com>
2025-06-19 22:52:27 +0200

    Fix the Password Visibility Toggle

--- resources/views/components/auth/password.blade.php
+++ resources/views/components/auth/password.blade.php
@@ -2,13 +2,15 @@
 
 <div class="mb-3">
     <label class="form-label">{{ $label }}</label>
-    <input type="{{ $showModel ? 'text' : 'password' }}" wire:model.defer="{{ $model }}" class="form-control"
+    <input type="{{ $this->$showModel ? 'text' : 'password' }}" wire:model.defer="{{ $model }}" class="form-control"
         placeholder="{{ $label }}" required>
+
     @error($model)
         <div class="text-danger small">{{ $message }}</div>
     @enderror
+
     <div class="form-check mt-2">
-        <input class="form-check-input" type="checkbox" wire:model.live.debounce.10ms="{{ $showModel }}"
+        <input type="checkbox" class="form-check-input" wire:model.live.debounce.10ms="{{ $showModel }}"
             id="toggle_{{ $model }}">
         <label class="form-check-label" for="toggle_{{ $model }}">Show password</label>
     </div>
