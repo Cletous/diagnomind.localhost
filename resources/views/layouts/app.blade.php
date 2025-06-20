f8ba15f18e3c0c8fccfcc52eafee9d0a67b31263
Cletous <ngomacletousjnr@gmail.com>
2025-06-20 13:06:18 +0200

    Added sweet alert CSS

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,6 +7,10 @@
     </title>
     <link href="{{ asset('assets/css/bootstrap.min.css') }}" rel="stylesheet">
     <link href="{{ asset('assets/icons/bootstrap-icons/bootstrap-icons.min.css') }}" rel="stylesheet">
+    <!-- Sweet Alert -->
+    <script src="{{ asset('assets/plugins/sweetalert2/js/sweetalert2.all.min.js') }}"></script>
+    <link rel="stylesheet" href="{{ asset('assets/plugins/sweetalert2/css/sweetalert2.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('assets/plugins/sweetalert2/css/styles.css') }}">
     @livewireStyles
 </head>
 
