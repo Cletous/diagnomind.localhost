3f1fb728ec98edd624d3ad833bdae11155a90b7b
Cletous <ngomacletousjnr@gmail.com>
2025-06-29 20:01:43 +0200

    Update HospitalsSeeder.php

--- database/seeders/HospitalsSeeder.php
+++ database/seeders/HospitalsSeeder.php
@@ -15,6 +15,7 @@ class HospitalsSeeder extends Seeder
 
         $hospitals = [
             [
+                'id' => 1,
                 'name' => 'Harare General Hospital',
                 'address' => '263 Josiah Tongogara St, Harare, Zimbabwe',
                 'phone' => '+263242707011',
@@ -22,6 +23,7 @@ class HospitalsSeeder extends Seeder
                 'admin_id' => $doctor1->id,
             ],
             [
+                'id' => 2,
                 'name' => 'Parirenyatwa Group of Hospitals',
                 'address' => 'Mazowe Street, Harare, Zimbabwe',
                 'phone' => '+263242701555',
@@ -29,6 +31,7 @@ class HospitalsSeeder extends Seeder
                 'admin_id' => $doctor2->id,
             ],
             [
+                'id' => 3,
                 'name' => 'Chitungwiza Central Hospital',
                 'address' => 'Off Seke Road, Chitungwiza, Zimbabwe',
                 'phone' => '+263772458221',
@@ -36,6 +39,7 @@ class HospitalsSeeder extends Seeder
                 'admin_id' => $doctor1->id,
             ],
             [
+                'id' => 4,
                 'name' => 'Mpilo Central Hospital',
                 'address' => 'Luveve Road, Bulawayo, Zimbabwe',
                 'phone' => '+263219887651',
@@ -43,6 +47,7 @@ class HospitalsSeeder extends Seeder
                 'admin_id' => $doctor2->id,
             ],
             [
+                'id' => 5,
                 'name' => 'Mutare Provincial Hospital',
                 'address' => 'Aerodrome Rd, Mutare, Zimbabwe',
                 'phone' => '+263270642204',
@@ -53,8 +58,9 @@ class HospitalsSeeder extends Seeder
 
         foreach ($hospitals as $hosp) {
             $hospital = Hospital::firstOrCreate(
-                ['name' => $hosp['name']],
+                ['id' => $hosp['id']],
                 [
+                    'name' => $hosp['name'],
                     'address' => $hosp['address'],
                     'phone' => $hosp['phone'],
                     'email' => $hosp['email'],
