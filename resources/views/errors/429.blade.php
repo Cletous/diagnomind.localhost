e2430644efa295005be1b6764e7af774376e86e5
Cletous <ngomacletousjnr@gmail.com>
2025-06-29 20:32:38 +0200

    Update 429.blade.php

--- resources/views/errors/429.blade.php
+++ resources/views/errors/429.blade.php
@@ -1,1 +1,10 @@
-<x-layouts.blank.app></x-layouts.blank.app>
+<x-layouts.blank.app>
+    <div class="container py-5 text-center">
+        <h1 class="display-1 text-warning">429</h1>
+        <p class="lead">Easy there! You're overdosing on clicks 💉</p>
+        <p class="text-muted">You've made too many requests in a short time. Please take a break and try again later.</p>
+        <a href="{{ route('home') }}" class="btn btn-outline-warning mt-3">
+            Back to Safety
+        </a>
+    </div>
+</x-layouts.blank.app>
